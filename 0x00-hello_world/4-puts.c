af61a2958ca62fc21bc73a6d14dd0b08ddb5dd19
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-07 17:53:25 -0700

    Yarab haniit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
   * Descreption: A C program that printes with put function
   *
   * Return Always 0 (success)
-  */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
