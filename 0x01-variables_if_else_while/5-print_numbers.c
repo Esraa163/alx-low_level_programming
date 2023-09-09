02cfc896e35008192e086296288ae40c93797483
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-09 04:57:21 -0700

    yarab haniit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit <=9; digit++)
-		ptintf("%d",digit);
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d", digit);
 	printf("\n");
 
 	return (0);
