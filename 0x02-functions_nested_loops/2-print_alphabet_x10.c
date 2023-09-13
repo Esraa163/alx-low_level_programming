82acfae7f1f6c71f0aa3d36e8d33a88af0b5103c
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-12 18:38:45 -0700

    yarab haniit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - that print the alphabets from a - z but 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 9; i++)
+		print_alphabet();
+}
