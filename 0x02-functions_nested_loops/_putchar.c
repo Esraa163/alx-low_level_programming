09271b4bcb3b9bd18c43a5cfee748ca9b2ae6204
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-12 17:44:21 -0700

    yarab haniit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * __putchar - write the caracter c to stdout
+ * _putchar - write the caracter c to stdout
  * @c: the caracter to print
  *
  * Return: 1 (success)
