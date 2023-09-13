b4eb255c260a91c5eaf6c8dcd16beb8c66027f83
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-12 19:01:04 -0700

    yarab haniit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to print all alphabets as lowercase or uppercase
+ *
+ * @c: takes input from other function
+ *
+ * Return: the function return (1) for any alphabet and return 0 in otherwise
+ *
+*/
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
