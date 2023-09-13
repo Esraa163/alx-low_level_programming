86f111262826cc31e783b9382d73077c6e50deda
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-12 19:21:46 -0700

    yarab haniit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the abslute
+ *		value of an integer
+ *
+ * @n: takes in integer type onput for the function
+ *
+ * Return: allwya 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+}
