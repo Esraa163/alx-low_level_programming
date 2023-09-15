6e1b071a6b58126ee5fc19c9a41ed96496ceccee
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-15 16:38:05 -0700

    yarab haniit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,15 @@
 /**
  * _isupper - print 1 if upper and 0 if lower
  *
- * @c: is the char tatwill checked
+ * @c: is the char that will checked
  *
  * Return: return 1 if upper return 0 if lower
 */
 
 int _isupper(int c)
 {
-	if (c > 96 && c < 123)
-	{
-		return (0);
-	}
-	if (c > 64 && c < 91)
-	{
+	if (c >= 48 && c < 57)
 		return (1);
-	}
+	else
+		return (0);
 }
