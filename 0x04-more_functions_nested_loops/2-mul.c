96f3c6f8c6a33352f839b04f6b9929d2b0052175
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-15 16:57:56 -0700

    yarab haniit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - that multibly two numbers
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: always (a * b) where a and b is two numbers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
