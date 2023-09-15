fca826bce5f3b9e7ec6956f08b39e29776c79117
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-15 16:06:57 -0700

    yarab haniit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * */
+
+int _isupper(int c)
+{
+	if (c > 96 && c < 123)
+  {
+	  return (0);
+  }
+  if (c > 64 && c < 91)
+  {
+	  return (1);
+  }
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write the caracter c to stdout
+ * @c: the caracter to print
+ *
+ * Return: 1 (success)
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
