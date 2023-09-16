e2251b13b18aeb39795e8448b0fd995ba12304da
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-15 18:48:39 -0700

    yarab haniit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,22 +17,25 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if ((num % 3 == 0) && !(num % 5 == 0))
 		{
-			printf("FizzBuzz ");
-			continue;
+			printf("Fizz");
 		}
-		if (num % 3 == 0)
+		else if ((num % 5 == 0) && !(num % 3 == 0))
 		{
-			printf("Fizz ");
-			continue;
+			printf("Buzz");
 		}
-		if (num % 5 == 0)
+		else if ((num % 5 == 0) && (num % 3 == 0))
 		{
-			printf("Buzz ");
-			continue;
+			printf("FizzBuzz");
 		}
-		printf("%d ", num);
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
