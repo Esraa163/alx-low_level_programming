35561ccc57542b0e64f6b671db20bcd48c742123
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-15 18:32:59 -0700

    yarab haniit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: function print the number
+ *		1 to 100 but if div by 3
+ *		will print Fizz if div by 5
+ *		will print Buzz
+ *
+ * Return: return (0) (succsses)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		if (num % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", num);
+	}
+	return (0);
+}
