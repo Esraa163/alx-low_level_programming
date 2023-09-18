dfbce0c2ebcf1a2081a9bd80a483e12e340e7596
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-18 15:41:59 -0700

    yarab haniit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - print the string
+ *
+ * @str: the input pointer to function
+*/
+
+void _puts(char *str)
+{
+	for (str = 0; *str != '\0'; str++)
+		return (*str);
+	_putchar('\n');
+}
