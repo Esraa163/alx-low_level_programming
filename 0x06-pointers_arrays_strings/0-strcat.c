74b8d25d0da0812dbe2781c7e061de47e5debc59
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-22 14:44:00 -0700

    yarab haniit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,12 @@ char *_strcat(char *dest, char *src)
 	int c1, c2;
 
 	c1 = 0;
-	while (dest[c1])
+	while (dest[c1] != '\0')
 		c1++;
 
 	for (c2 = 0; src[c2] != '\0'; c2++)
 		dest[c1++] = str[c2];
 
+	dest[c1] = '\0';
 	return (dest);
 }
