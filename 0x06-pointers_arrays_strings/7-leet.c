88909aaec9b05cf5027052137caa4e65a2c00cb9
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-22 16:05:59 -0700

    yarab haniit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,26 @@
 #include "main.h"
 
 /**
- * */
+ * *leet - function that encode letters
+ * @a: th input
+ * Return: the letters that is coded
+*/
+
+char *leet(char *a)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711"
+
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				if (a[i] == s1[j])
+				{
+					a[i] = s2[j];
+				}
+			}
+		}
+	return (a);
+}
