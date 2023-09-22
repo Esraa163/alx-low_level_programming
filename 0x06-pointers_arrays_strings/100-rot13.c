7a8daafcccceed5a8834397e462752a7d64dab71
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-22 16:25:50 -0700

    yarab haniit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - will rev as in ASCII
+ * @a: input
+ * Return: the value
+*/
+
+char *rot13(char *a)
+{
+	int i, j;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHILKLM";
+
+	while (a[i])
+	{
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			for (j = 0; j <= 52; j++)
+			{
+				if (a[i] == rot13[j])
+					a[i] = ROT1[j];
+			}
+		}
+
+	}
+	return (a);
+}
