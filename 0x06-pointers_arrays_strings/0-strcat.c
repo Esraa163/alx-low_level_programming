72413b5c7192bad9e5b19e7f1db73ca077a6f288
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-22 14:30:34 -0700

    yarab haniit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcat - pointer that will make
+ *
+ *@src: pointer input that taken
+ *@dest: pointer input that will be returned
+ *
+ * Return: the dest value
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c1])
+		c1++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c1++] = str[c2];
+	return (dest);
+}
