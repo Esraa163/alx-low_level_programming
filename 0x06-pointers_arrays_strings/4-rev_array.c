1bc098e43f19488aa7b4ffef13d12fdc59c24498
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-22 15:29:12 -0700

    yarab haniit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
- * */
+ * reverse_array - function that will rev the array
+ *
+ * @a: input array
+ * @n: size of array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
