0e93f33f9d62f3958b05a6b6e57d8b8a564ff006
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-25 19:03:27 -0700

    yarab haniit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * *_memset - work as the function memset
+ *
+ * @s: input string
+ *
+ * @b: constant
+ *
+ * @n: max number of bytes
+ * Return: string s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
