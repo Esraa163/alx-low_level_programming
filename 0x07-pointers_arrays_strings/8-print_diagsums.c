59fdb5be307fa3af294e3123de1cf1d314f2b412
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-25 19:52:59 -0700

    yarab haniit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
 /**
- * */
+ * print_diagsums - prints the diagsums
+ * @a: input array
+ * @size: array's size
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, " s1);
+	printf("%d\n" s2);
+}
