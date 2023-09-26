088e9c5f9f9cdc50eaf70f517cb9046e8d4788d2
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-25 19:45:51 -0700

    yarab haniit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strstr - tack the second string after the char found 
+ * @haystack: input string;
+ * @needle: second string
+ * Return: nedeel with haystack
+*/
+
+char *_strstr(char *haystack, char *needle)
+{}
