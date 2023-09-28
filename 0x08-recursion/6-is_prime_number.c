0c57df8a6341daaccafd9f62f4f962b7c79ad07b
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-09-28 16:22:22 -0700

    yarab haniit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,24 +6,24 @@
  * @m: input number
  * Return: the value
 */
-int check_prrime(int n, int m);
+int check_prime(int n, int m);
 int is_prime_number(int n)
 {
 	return (check_prime(n, 2));
 }
 
 /**
- * check_prrime - functhion to check
+ * check_prime - functhion to check
  * @n: number
  * @m: anter number
  * Return: int
 */
-int check_prrime(int n, int m)
+int check_prime(int n, int m)
 {
 	if (m >= n && n > 1)
 		return (1);
 	else if (n % m == 0 || n <= 1)
 		return (0);
 	else
-		return (check_prrime(n, m + 1));
+		return (check_prime(n, m + 1));
 }
