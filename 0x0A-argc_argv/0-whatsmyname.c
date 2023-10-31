4ba87680df2b09736fef86aa1d5590680931e1b5
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-10-30 17:25:37 -0700

    2 month and not the last Yaraab

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - piging of the program
+ *
+ * @argc: input of the size of argv
+ *
+ * @argv: the string input
+ *
+ * Return: (0) if true
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
