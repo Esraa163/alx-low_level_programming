99efe91854471b74fd278bce87e9ad7701ba7a3d
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-10-30 18:22:29 -0700

    MY second month YAaarb Not last one

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the sum of 2 num
+ * @argc: number of command line input
+ * @argv: the string of the command line input
+ * Return: 0 if true
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(2);
+		printf("%d\n", sum)
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
