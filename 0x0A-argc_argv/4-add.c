b2911c4cec9a1a6b4e1dbd8642a68011d1369e60
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-10-30 18:36:02 -0700

    MY second month YAaarb Not last one

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 
-/***
- * main - function that print/
+/**
+ * main - all starts here
+ * @argv: the string that main taken
+ * @argc: the number of command line
+ * Return: 0 if true
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
