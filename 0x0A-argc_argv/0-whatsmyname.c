8f6ee033d856f2d1dfe6d9c878806d4f961be934
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-10-30 19:00:07 -0700

    MY second month YAaarb Not last one

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,7 +11,7 @@
  * Return: (0) if true
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char const *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
