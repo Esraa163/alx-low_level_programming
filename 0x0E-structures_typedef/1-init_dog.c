c09887d8111fa02a387245e8e22cf5809458ecf7
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-11-06 19:31:31 -0800

    FOR MY second MONth YArab HAniit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * init_dog - for initialize a dog
+ * @d: the dog to init
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: just a lone doge struct in big kitty world
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
