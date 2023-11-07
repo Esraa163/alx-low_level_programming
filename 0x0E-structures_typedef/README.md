d072c4c953fdf5fa960de484dd080b04273e4da3
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-11-06 19:26:01 -0800

    FOR MY second MONth YArab HAniit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+ESRAA IS HEAR
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - adog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: just a lone doge struct in big kitty world
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
