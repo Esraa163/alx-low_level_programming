cb267a4cf4b4e93a7c60579ac2e2a3c3a34fb392
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-11-28 04:17:26 -0800

    MY second MOUNth YOU CAN DOOooo IT

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,19 @@ char *_strdup(char *str)
 	int i = 0, size = 0;
 	char *m;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (; str[size] != '\0' ; size++)
 	;
-	
-	m = malloc(size *sizeof(*str) + 1);
+
+	m = malloc(size * sizeof(*str) + 1);
 
 		if (m == 0)
+		{
 			return (NULL);
-		else 
+		}
+		else
 		{
 			for (; i < size; i++)
 				m[i] = str[i];
