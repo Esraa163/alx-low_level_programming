952c7c74f0797da7a9c94608d17f68d56fc5fc44
Esraa Abdellatif <esraa.abdellatif16@gmail.com>
2023-11-28 05:12:45 -0800

    MY second MOUNth YOU CAN DOOooo IT

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	}
 	else
 	{
-		for (i = 0; i< height; i++)
+		for (i = 0; i < height; i++)
 		{
 			tab[i] = malloc(sizeof(**tab) * width);
 			if (tab[i] == 0)
